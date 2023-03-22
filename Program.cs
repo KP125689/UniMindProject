991f1606b90001ae005f85df30844263d353c30a
KP125689 <P.RWEHUMBIZA-2021@hull.ac.uk>
2023-03-22 13:16:33 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySQL.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace UniMindProject
+{
+    class theDatabase
+    {
+        //this is where the code for the database will be held
+        /// <database> 
+        /// I, Paatrick, am wririntg the code on a seperate solution so i can test the database and make sure it fully works before
+        /// transferring it here.
+        /// </database>
+        /// <returns></returns>
+
+    }
+
+
+
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
