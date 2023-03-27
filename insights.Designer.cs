dcec2c9310fe575f31fb76cf0e234de4153e8dc9
KP125689 <P.RWEHUMBIZA-2021@hull.ac.uk>
2023-03-28 00:43:17 +0100

    update2

--- insights.Designer.cs
+++ insights.Designer.cs
@@ -36,17 +36,19 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(100, 69);
+            this.richTextBox1.Location = new System.Drawing.Point(132, 85);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(178, 100);
+            this.richTextBox1.Size = new System.Drawing.Size(236, 122);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(100, 175);
+            this.button1.Location = new System.Drawing.Point(133, 215);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 65);
+            this.button1.Size = new System.Drawing.Size(237, 80);
             this.button1.TabIndex = 1;
             this.button1.Text = "Ask me";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,30 +57,33 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(97, 42);
+            this.label1.Location = new System.Drawing.Point(129, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.Size = new System.Drawing.Size(250, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "tell me your problem and i will advise you";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(168, 283);
+            this.label2.Location = new System.Drawing.Point(224, 348);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.Size = new System.Drawing.Size(25, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "......";
             // 
             // insights
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 450);
+            this.ClientSize = new System.Drawing.Size(523, 554);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "insights";
             this.Text = "insights";
             this.ResumeLayout(false);
